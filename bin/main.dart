e50fef2f0d5a1adee24a0fd383cae39ec0cd3cb1
Amin anwar <baseljaber2019@gmail.com>
2025-05-17 13:07:43 +0300

    Create main entry point to run the Tic Tac Toe game

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,9 @@
+import 'package:tic_tac_toe_game/game.dart';
 
-void main(List<String> arguments) {
+void main() {
+  // Create a new game instance
+  Game game = Game();
+
+  // Start the game
+  game.start();
 }
